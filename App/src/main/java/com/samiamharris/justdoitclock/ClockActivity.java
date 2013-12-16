69806e24a1b5a6482a2a578a22a1fe0d4081ce81
Sam Harris <sharris@trinity.edu>
2013-12-16 16:29:07 -0600

    .gitignore file added
    Edit and Add buttons work on Clock Frag

--- App/src/main/java/com/samiamharris/justdoitclock/ClockActivity.java
+++ App/src/main/java/com/samiamharris/justdoitclock/ClockActivity.java
@@ -3,6 +3,7 @@ package com.samiamharris.justdoitclock;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,16 +12,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class ClockActivity extends Activity {
+public class ClockActivity extends Activity implements ClockFragment.OnAddEditListener {
+
+
+    // What will this class need to do?
+    // go directly to the clock fragment on start up
+    // communicate between fragments (like the headline/article)
+    // pass data between fragments (something with bundle)
+    // not sure if this will have the alarm manager
+
+    AlarmData[] arrayAlarmData = {new AlarmData(),
+    new AlarmData(),
+    new AlarmData()};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.clock_activity);
 
+        ClockFragment clockFragment = new ClockFragment();
+
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, clockFragment)
                     .commit();
         }
     }
@@ -28,7 +42,7 @@ public class ClockActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.clock, menu);
         return true;
@@ -46,20 +60,29 @@ public class ClockActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
 
-        public PlaceholderFragment() {
-        }
+    @Override
+    public void onAddSelected() {
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.clock_fragment, container, false);
-            return rootView;
-        }
+        AddFragment addFragment = new AddFragment();
+
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, addFragment)
+                .addToBackStack(null)
+                .commit();
+     }
+
+    @Override
+    public void onEditSelected() {
+
+        EditFragment editFragment = new EditFragment();
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, editFragment )
+                .addToBackStack(null)
+                .commit();
+
+
     }
-
 }
