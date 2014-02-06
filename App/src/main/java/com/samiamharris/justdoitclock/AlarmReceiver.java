fe6c0b0c5683e55cd54f2e2e64215ef0623eabe3
Sam Harris <sharris@trinity.edu>
2014-02-06 15:38:13 -0600

    Notification returns to phrase fragment when alarm goes off

--- App/src/main/java/com/samiamharris/justdoitclock/AlarmReceiver.java
+++ App/src/main/java/com/samiamharris/justdoitclock/AlarmReceiver.java
@@ -1,8 +1,12 @@
 package com.samiamharris.justdoitclock;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.util.Log;
@@ -60,6 +64,23 @@ public class AlarmReceiver extends BroadcastReceiver{
 
                 try
                 {
+
+                    PendingIntent pi = PendingIntent.getActivity(context, 0, new Intent(context, ClockActivity.class), 0);
+
+                    Notification notification = new Notification.Builder(context)
+                            .setTicker(receivedAlarm.getmPhrase())
+                            .setSmallIcon(R.drawable.ic_launcher)
+                            .setContentTitle(receivedAlarm.getmName())
+                            .setContentText(receivedAlarm.getmPhrase())
+                            .setContentIntent(pi)
+                            .setAutoCancel(true)
+                            .setLights(Color.BLUE,1000,1000)
+                            .build();
+
+                    NotificationManager notificationManager = (NotificationManager)
+                            context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+                    notificationManager.notify(0, notification);
 
                     Intent alarmIntent = new Intent( context, ClockActivity.class );
                     //alarmIntent.addFlags(Intent.FLAG_FROM_BACKGROUND);
